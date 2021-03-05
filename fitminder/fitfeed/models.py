8e94e4ed06d69e269e4f8abf74353e2d5d3573f2
PieroH <piero.herbec@gmail.com>
2021-03-05 13:07:53 +0100

    Admin Page, Init model for FitFeed, First templates

--- fitminder/fitfeed/models.py
+++ fitminder/fitfeed/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Feed(models.Model):
+	
+	f1 = 'fa1'
+	f2 = 'fa2'
+	f3 = 'fa3'
+	f4 = 'fa4'
+
+	FIT_ACTIVITIES = [
+		(f1, 'Fat Burn'),
+		(f2, 'Cardio'),
+		(f3, 'Strength'),
+		(f4, '...')
+	]
+
+	fit_title = models.CharField(max_length=100)
+	fit_content = models.CharField(
+		max_length=3,
+		choices=FIT_ACTIVITIES, 
+		default=f4
+	)
+	fit_duration = models.DurationField()
+	datetime_feed = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	fit_image = models.ImageField(default='#', upload_to='activity_thumbnail')
+
+
+	def __str__(self):
+		return self.title
